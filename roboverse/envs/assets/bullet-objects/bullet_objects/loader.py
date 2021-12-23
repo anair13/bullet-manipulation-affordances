4e2c93c349fdff97921850bb00c6036a5753bc5d
Ashvin Nair <nair.ashvin@gmail.com>
2021-12-22 16:19:34 -0800

    Add python loader file

---
+++ roboverse/envs/assets/bullet-objects/bullet_objects/loader.py
@@ -0,0 +1,24 @@
+import numpy as np
+from roboverse.bullet.misc import load_obj, deg_to_quat
+import os.path as osp
+from pathlib import Path
+
+path = Path(__file__).parent.absolute()
+
+SHAPENET_ASSET_PATH = str(path.parent.joinpath("ShapeNetCore"))
+print("shapenet asset path:", SHAPENET_ASSET_PATH)
+
+def load_shapenet_object(object_path, scaling, object_position, scale_local=0.5, rgba=None, quat=None):
+    path = object_path.split('/')
+    dir_name, object_name, = path[-2], path[-1]
+
+    # Randomize initial theta
+    if quat is None:
+        quat = deg_to_quat(np.random.randint(0, 360, size=3))
+
+    obj = load_obj(
+        SHAPENET_ASSET_PATH + '/ShapeNetCore_vhacd/{0}/{1}/model.obj'.format(dir_name, object_name),
+        SHAPENET_ASSET_PATH + '/ShapeNetCore.v2/{0}/{1}/models/model_normalized.obj'.format(dir_name, object_name),
+        object_position, quat=quat, rgba=rgba, scale=scale_local*scaling['{0}/{1}'.format(dir_name, object_name)])
+
+    return obj
